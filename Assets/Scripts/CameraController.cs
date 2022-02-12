ea92e7fbe56707e8e381d15fdb2a3c4a6639e7e8
Greger Sundvall <gsundvall@gmail.com>
2022-02-12 20:17:54 +0100

    purrdy najs follow cam

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,41 +9,39 @@ public class CameraController : MonoBehaviour
 
     private Rigidbody _carRB;
     private CarController _carController;
-    private Vector3 _offsetPosition = new Vector3(0, 2, 0);
+    private Vector3 _offset = new Vector3(0, 1.5f, 0);
     private float _xOffset;
     private float _xMinOffset = 0;
     private float _xMaxOffset = 2;
     private float _zOffset;
     private float _zMinOffset = 0;
     private float _zMaxOffset = 2;
-    
+
+    private Vector3 _rotation;
     
     private void Awake()
     {
         _carRB = car.GetComponent<Rigidbody>();
         _carController = car.GetComponent<CarController>();
+        _rotation = transform.rotation.eulerAngles;
     }
 
     private void Update()
     {
-        SetOffsetPosition();
-        
-        transform.rotation = car.transform.rotation;
-        transform.position = car.transform.TransformPoint(GetOffsetPosition());
-        
+        SetOffset();
+        transform.position = car.transform.TransformPoint(_offset);
+        transform.rotation = Quaternion.LookRotation(car.transform.position - transform.position);
     }
     
-    public void SetOffsetPosition()
+    private void SetOffset()
     {
-        _xOffset = Mathf.MoveTowards(_xOffset, -_carController.Steering, 0.02f);
         _zOffset = -1.5f + -_carRB.velocity.magnitude * 0.02f;
+        _offset.z = _zOffset;
+
+        _xOffset = Mathf.MoveTowards(_xOffset, -_carController.Steering, 0.02f);
+        _offset.x = _xOffset;
     }
     
-    private Vector3 GetOffsetPosition()
-    {
-        _offsetPosition.x = _xOffset;
-        _offsetPosition.z = _zOffset;
-        return _offsetPosition;
-    }
+    
 
 }
